8c1b2f5b22d95268332fa6a58b9bef3d2ff4506d
Aswin M <50163512+asnzodiac@users.noreply.github.com>
2025-05-13 13:11:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
-const cacheName = "aix-sec-cok-v1";
-const assetsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
+const CACHE_NAME = "aix-flight-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Install SW and cache static assets
-self.addEventListener("install", (event) => {
+// Install event: Cache app shell
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(assetsToCache);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Serve cached content when offline
-self.addEventListener("fetch", (event) => {
+// Fetch event: Serve cached content if offline
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
